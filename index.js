a5f94563e6e56ab7adeeb89b0d2191c75eea0ca7
santiagoquilindo <santiagoquilindo32@gmail.com>
2025-02-20 17:01:14 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+app.use("/api/libros", require("./routes/libroRoutes"));
+app.use("/api/usuarios", require("./routes/usuarioRoutes"));
+app.use("/api/prestamos", require("./routes/prestamoRoutes"));
+
+// Puerto
+const PORT = 9099;
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor corriendo en puerto: ${PORT}`);
+});
